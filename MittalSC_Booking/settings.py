6e46b9e2f630fe7b1cbdae453de1006047dcc5aa
Vatsal Varshney <vatsalvar1211@gmail.com>
2022-09-01 01:19:38 +0530

    Hosted user files on AWS S3

--- MittalSC_Booking/settings.py
+++ MittalSC_Booking/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'slots',
     'crispy_forms',
     'profanity',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -148,3 +149,16 @@ MESSAGE_TAGS = {
     message_constants.WARNING: 'warning',
     message_constants.ERROR: 'danger',
 }
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_REGION_NAME = 'ap-south-1'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_ADDRESSING_STYLE = "virtual"
