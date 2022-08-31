22a8bedc63eda36e7359328bc90d0338ae00d43f
Vatsal Varshney <vatsalvar1211@gmail.com>
2022-09-01 02:08:55 +0530

    Minor change in profile page

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm, PfpChangeForm
 from .models import User, Role
+from slots.views import context
 from django.contrib import messages
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
@@ -26,6 +27,7 @@ def register(request):
 
 @login_required(login_url='login')
 def profile(request, username):
+    cont=context(request)
     if User.objects.filter(username=username):
         view_user=User.objects.get(username=username)
         if view_user.is_active:
@@ -47,7 +49,9 @@ def profile(request, username):
             paginator = Paginator(bookings_list, 7)
             page_number = request.GET.get('page')
             page_obj = paginator.get_page(page_number)
-            return render(request, 'users/profile.html', {'view_user': view_user, 'page_obj':page_obj})
+            cont['view_user']=view_user
+            cont['page_obj']=page_obj
+            return render(request, 'users/profile.html', cont)
         else:
             return render(request, 'users/no-profile.html', {'exists': True})
     else:
